30315cd7708c45172df1687c17b47592e419e808
Spiritofthecore <cuongdepgiai1998@gmail.com>
2019-11-08 22:31:34 +0700

    test commit

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
+1+1;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
