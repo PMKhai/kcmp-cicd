4b77fdec3be84c60045924ed9b3d4dc3d9e3b166
Spiritofthecore <cuongdepgiai1998@gmail.com>
2019-11-23 22:11:45 +0700

    add test

---
+++ test/bootstrap.js
@@ -0,0 +1,32 @@
+
+const puppeteer = require('puppeteer');
+const { expect } = require('chai');
+const _ = require('lodash');
+const globalVariables = _.pick(global, ['browser', 'expect']);
+
+// puppeteer options
+const opts = {
+  headless: false,
+  slowMo: 100,
+  timeout: 10000
+};
+
+// expose variables
+before (function (done) {
+  global.expect = expect;
+
+  puppeteer
+    .launch(opts)
+    .then(function (browser) {
+      global.browser = browser;
+      done();
+    });
+});
+
+// close browser and reset global variables
+after (function () {
+  browser.close();
+
+  global.browser = globalVariables.browser;
+  global.expect = globalVariables.expect;
+});
--- test/test.js
+++ test/test.js
@@ -1,8 +1,53 @@
-var request = require('supertest');
-var app = require('../bin/www');
-describe('GET /', function() {
- it('respond with hello world', function(done) {
- //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('hello world', done);
- });
+// var request = require('supertest');
+// var app = require('../app.js');
+// describe('GET /', function() {
+//  it('respond with hello world', function(done) {
+//  //navigate to root and check the the response is "hello world"
+//  request(app).get('/').expect('hello world', done);
+//  });
+// });
+const { expect } = require('chai');
+//
+// describe('sample test', function () {
+//   it('should work', async function () {
+//     console.log(await browser.version());
+//
+//     expect(true).to.be.true;
+//   });
+// });
+
+describe('sample test', function () {
+  let page;
+
+  before (async function () {
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000');
+  });
+
+  after (async function () {
+    await page.close();
+  })
+
+  it('should have the correct page title', async function () {
+    expect(await page.title()).to.eql('demo cicd');
+    done();
+  });
+
+  // it('should have a heading', async function () {
+  //   const HEADING_SELECTOR = 'h1';
+  //   let heading;
+  //
+  //   await page.waitFor(HEADING_SELECTOR);
+  //   heading = await page.$eval(HEADING_SELECTOR, heading => heading.innerText);
+  //
+  //   expect(heading).to.eql('Page Title');
+  // });
+  //
+  // it('should have a single content section', async function () {
+  //   const BODY_SELECTOR = '.main-content';
+  //
+  //   await page.waitFor(BODY_SELECTOR);
+  //
+  //   expect(await page.$$(BODY_SELECTOR)).to.have.lengthOf(1);
+  // });
 });
